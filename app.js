17f62aae69f767865b8bcb9dbb27fcdcdccdbe83
danieljackson31@hotmail.com <47636562+Ped4ntic@users.noreply.github.com>
2023-09-12 00:04:50 +0100

    style and layout still need attention

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 const container = document.querySelector('.grid-container');
+const gridSizeSlider = document.getElementById('gridSizeSlider');
+const sliderValueElement = document.getElementById('sliderValue');     
+
+const colorPicker = document.getElementById('colorPicker');
+const colorBtn = document.getElementById('colorBtn');
+const randomColorBtn = document.getElementById('rainbowBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+
+let activeButton = colorBtn;
+let isDragging = false; 
+
+const clearBtn = document.getElementById('clearBtn');
+
+let colorForBG = "#b9bfc0";
+let isRandomColor = false;
+
+colorBtn.addEventListener('click', function() {
+    setActiveButton(colorBtn);
+    colorForBG = colorPicker.value; // Change color to red (you can set it to any color)
+    isRandomColor = false;
+});
+
+randomColorBtn.addEventListener('click', function() {
+    setActiveButton(randomColorBtn);
+    isRandomColor = true;
+});
+
+eraserBtn.addEventListener('click', function() {
+    setActiveButton(eraserBtn);
+    colorForBG = "#FFFFFF"; 
+    isRandomColor = false;
+});
+
+clearBtn.addEventListener('click', updateGridSize)
+gridSizeSlider.addEventListener('input', updateGridSize);
 
 console.log(container);
 
@@ -7,17 +42,56 @@ function createGrid(x){
         for(let col = 0; col < x; col++){
             let elem = document.createElement('div');
             elem.classList.add('grid');
+            elem.addEventListener('mousedown', startDrag);
+            elem.addEventListener('mousemove', handleDrag);
+            elem.addEventListener('mouseup', endDrag);
             container.append(elem); 
         }
     }
     const size = 960/x;
     container.style.gridTemplateColumns = `repeat(${x}, ${size}px)`;
     container.style.gridTemplateRows = `repeat(${x}, ${size}px)`;
-    console.log(size);
-    // document.querySelectorAll('.grid').forEach(grid => {
-    //     grid.style.gridTemplateColumns = `repeat(${x}, ${size}px), 1fr`;
-    //     grid.style.gridTemplateRows = `repeat(${x}, ${size}px), 1fr`;
-    // });
+    sliderValueElement.textContent = `${x} x ${x}`;
 }
 
-createGrid(2);
+function startDrag(event) {
+    isDragging = true;
+    changeBackgroundColor(event);
+}
+
+// While dragging
+function handleDrag(event) {
+    if (isDragging) {
+        changeBackgroundColor(event);
+    }
+}
+
+// End of the drag
+function endDrag() {
+    isDragging = false;
+}
+
+function updateGridSize() {
+    const newSize = gridSizeSlider.value;
+    let gridItems = document.querySelectorAll('.grid');
+    gridItems.forEach(grid => grid.style.backgroundColor = "#ffffff");
+    createGrid(newSize);
+}
+
+function changeBackgroundColor(event) {
+    // Generate a random color (you can replace this with any color)
+    if(isRandomColor) {
+        colorForBG = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
+    // Change the background color of the div
+    event.target.style.backgroundColor = colorForBG;
+}
+
+// Set the active button and remove active class from others
+function setActiveButton(button) {
+    activeButton.classList.remove('active');
+    button.classList.add('active');
+    activeButton = button;
+}
+
+createGrid(16);
